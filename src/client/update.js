ad3a2eccad187297d3d0801832779541abdbe13a
Maciej Krüger <mkg20001@gmail.com>
2019-10-27 13:06:29 +0100

    feat: expose rid, upgrade deps

--- src/client/update.js
+++ src/client/update.js
@@ -6,49 +6,50 @@ const getRandomID = () => crypto.randomBytes(16).toString('hex')
 
 const { validateAndEncode, encodeTxData } = require('./process')
 
-async function createTx (data, arweave) {
-  return arweave.createTransaction({
+async function createTx (data, arweave, rid) {
+  const tx = await arweave.createTransaction({
     data: encodeTxData(data)
   }, arweave.jwk)
+
+  tx.addTag('i', rid)
+  tx.rid = rid
+
+  return tx
 }
 
 // TODO: do some verification before creating the TX
 
 async function entryCreate (arweave, entry, tags, val) {
-  const tx = await createTx(await validateAndEncode(entry, val), arweave)
-  const rid = getRandomID()
+  const tx = await createTx(await validateAndEncode(entry, val), arweave, getRandomID())
 
   for (const tag in tags) { // eslint-disable-line guard-for-in
     tx.addTag(tag, tags[tag])
   }
 
-  tx.addTag('i', rid)
   tx.addTag('a', 'c')
 
   return tx
 }
 
 async function entryModify (arweave, entry, rid, tags, diff) {
-  const tx = await createTx(await validateAndEncode(entry, diff, true), arweave)
+  const tx = await createTx(await validateAndEncode(entry, diff, true), arweave, rid)
 
   for (const tag in tags) { // eslint-disable-line guard-for-in
     tx.addTag(tag, tags[tag])
   }
 
-  tx.addTag('i', rid)
   tx.addTag('a', 'e')
 
   return tx
 }
 
 async function entryDelete (arweave, entry, rid, tags) {
-  const tx = await createTx(Buffer.from(''), arweave) // TODO: add contents
+  const tx = await createTx(Buffer.from(''), arweave, rid) // TODO: add contents
 
   for (const tag in tags) { // eslint-disable-line guard-for-in
     tx.addTag(tag, tags[tag])
   }
 
-  tx.addTag('i', rid)
   tx.addTag('a', 'd')
 
   return tx
